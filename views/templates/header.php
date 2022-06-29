117c7b7d2c608b893931b9c4f5e58447484362bd
anastasiiaal <anastasia.alexeenko@gmail.com>
2022-06-29 15:43:23 +0200

    :lipstick: css started

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,5 +1,82 @@
 <header class="header" id="header">
     <div class="container">
-        
+        <div class="nav-wrapper dflex">
+            <a href=".">
+                <img src="./views/img/logo.svg" alt="Logo">
+            </a>
+            <nav>
+                <ul class="dflex">
+                    <li>
+                        <a href=".">Home</a>
+                    </li>
+                    <li>
+                        <a href=".">New releases</a>
+                    </li>
+                    <li>
+                        <a href=".">Categories</a>
+                        <div class="nav__categories">
+                            <ul>
+                                <li>
+                                    <a class="category-link" href=".">Action</a>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <a class="category-link" href=".">Action</a>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <a class="category-link" href=".">Action</a>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <a href=".">Action</a>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <a href=".">Action</a>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <a href=".">Action</a>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <a href=".">Action</a>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <a href=".">Action</a>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <a href=".">Action</a>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <a href=".">Action</a>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <a href=".">Action</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+            <form id="form">
+                <input type="text" placeholder="Search" id="search" class="search">
+            </form>
+        </div>
     </div>
 </header>
