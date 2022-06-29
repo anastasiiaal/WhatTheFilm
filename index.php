49d08975039b8938574cffbb8d8ef58e96d9e3a8
Warrengers74 <warrengers.dev@gmail.com>
2022-06-29 15:45:50 +0200

    123

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ include('views/templates/head.php');
 <body>
 <?php
     include('views/templates/header.php');
+    include('views/templates/footer.php');
 ?>
 </body>
 </html>
---
+++ views/templates/footer.php
@@ -0,0 +1,32 @@
+<footer>
+    <div class="container">
+        <div class="footer-sections">
+            <div class="footer-section1">
+                <img src="./views/img/logo.svg" alt="logo">
+            </div>
+
+            <div class="footer-section2">
+                <nav class="nav-footer">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">New releases</a></li>
+                        <li><a href="#">Categories</a></li>
+                    </ul>
+                </nav> 
+            </div>
+
+            <div class="footer-section3">
+                <a href="#"><img src="./views/img/instagram.svg" alt="instagram"></a>
+                <a href="#"><img src="./views/img/facebook.svg" alt="facebook"></a>
+                <a href="#"><img src="./views/img/twitter.svg" alt="twitter"></a>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <p>All rights reserved © Study project by Ana & Wawa</p>
+        </div>
+        
+
+        
+
+    </div>
+</footer>
