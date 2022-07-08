af4904aea44f6a21ec581a6ca693cb9812e50e35
anastasiiaal <anastasia.alexeenko@gmail.com>
2022-07-08 08:52:30 +0200

    :cat2: page categories | cards done

--- views/categories.php
+++ views/categories.php
@@ -6,20 +6,33 @@ include('templates/head.php');
     include('templates/header.php');
     include('templates/separator.php');
 ?>
+<?php
+    $getGenres = $movieDB->getGenres();
+    $selectedGenres = [];
+
+    $getFilmsByGenre = $movieDB->getFilmsByGenre('36,27');
+?>
 <section class="categories-list" id="categories-list">
     <div class="container dflex">
-        <?php for ($i = 0; $i < 20; $i++) { ?>
-        <p class="menu">Action</p>
+        <?php foreach($getGenres as $genre) { ?>
+        <p class="menu" id="<?= $genre['id'] ?>"><?= $genre['name'] ?></p>
         <?php } ?>
     </div>
 </section>
 <section class="films dflex">
     <div class="container dflex">
-        <?php 
-        for ($i = 0; $i<20; $i++) {
-            include('templates/card-movie.php');
-        }
-        ?>
+        <?php foreach($getFilmsByGenre as $film) { ?>
+            <a href="./film.php?id=<?= $film['id'] ?>" class="card-movie__link">
+                <div class="card-movie">
+                    <img src="<?= $film['poster_path'] === "https://image.tmdb.org/t/p/w500" ? 'img/poster.png' : $film['poster_path'];?>" alt="<?= $film['title'] ?>">
+                    <div class="card-info">
+                        <h4><?= $film['title'] ?></h4>
+                        <p class="txt-sm"><?= $film['year'] ?></p>
+                        <h4 class="<?php if ($film['vote_average'] >= 7) { echo "green";} else if ($film['vote_average'] < 8 && $film['vote_average'] >= 5) {echo "orange";} else { echo "red";} ?>"><?= $film['vote_average'] ?></h4>
+                    </div>
+                </div>
+            </a>
+        <?php } ?>
     </div>
     <button class="btn-primary">See more films</button>
 </section>
--- views/templates/head.php
+++ views/templates/head.php
@@ -1,3 +1,6 @@
+<?php
+    require_once '../index.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
