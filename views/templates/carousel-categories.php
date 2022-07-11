e6e8078cebf3d7470eb39711eebcfc52b530719c
Warrengers74 <warrengers.dev@gmail.com>
2022-07-11 11:28:07 +0200

    data carousel categories

--- views/templates/carousel-categories.php
+++ views/templates/carousel-categories.php
@@ -1,27 +1,11 @@
 <?php
-$getFilmsByGenre = $movieDB->getFilmsByGenre('28');
+$getGenres = $movieDB->getGenres();
 ?>
-<div class="container">
 
-    <div class="slider" data-slick='{"slidesToShow": 6, "slidesToScroll": 1, "autoplay": true, "autoplaySpeed": 2000}'>
-        <a href="./categories.php"><div class="img-div div1"><h2>Action</h2></div></a>
-        <a href="./categories.php"><div class="img-div div2"><h2>Adventure</h2></div></a>
-        <a href="./categories.php"><div class="img-div div3"><h2>Animation</h2></div></a>
-        <a href="./categories.php"><div class="img-div div4"><h2>Comedy</h2></div></a>
-        <a href="./categories.php"><div class="img-div div5"><h2>Crime</h2></div></a>
-        <a href="./categories.php"><div class="img-div div6"><h2>Docu</h2></div></a>
-        <a href="./categories.php"><div class="img-div div7"><h2>Drama</h2></div></a>
-        <a href="./categories.php"><div class="img-div div8"><h2>Family</h2></div></a>
-        <a href="./categories.php"><div class="img-div div9"><h2>Fantasy</h2></div></a>
-        <a href="./categories.php"><div class="img-div div10"><h2>History</h2></div></a>
-        <a href="./categories.php"><div class="img-div div11"><h2>Horror</h2></div></a>
-        <a href="./categories.php"><div class="img-div div12"><h2>Music</h2></div></a>
-        <a href="./categories.php"><div class="img-div div13"><h2>Mystery</h2></div></a>
-        <a href="./categories.php"><div class="img-div div14"><h2>Romance</h2></div></a>
-        <a href="./categories.php"><div class="img-div div15"><h2>Sci-Fi</h2></div></a>
-        <a href="./categories.php"><div class="img-div div16"><h2>TV Movie</h2></div></a>
-        <a href="./categories.php"><div class="img-div div17"><h2>Thriller</h2></div></a>
-        <a href="./categories.php"><div class="img-div div18"><h2>War</h2></div></a>
-        <a href="./categories.php"><div class="img-div div19"><h2>Western</h2></div></a>
+<div class="container">
+    <div class="slider">
+        <?php for ($i = 0; $i < 19; $i++) { ?>
+            <a href="./categories.php?idgenre=<?= $getGenres[$i]['id'] ?>" class="img-div"><img src="./img/categories/<?= $getGenres[$i]['name'] ?>.jpg" alt=""><h2><?= $getGenres[$i]['name'] === "Documentary" ? $getGenres[$i]['name'] = "Docu" : $getGenres[$i]['name'] ?></h2></a>
+        <?php } ?>
     </div>
 </div>
