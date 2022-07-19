f3664b8f5d6d7b53e62b19599581c08e5843e5f7
anastasiiaal <anastasia.alexeenko@gmail.com>
2022-07-19 10:46:30 +0200

    :recycle: code cleanup

--- views/templates/pagename.php
+++
@@ -1,6 +0,0 @@
-<?php
-// helps retrieving the name of the current page to insert the correct h2 in the separation line
-$linkArray = strval($_SERVER['PHP_SELF']);
-$linkArrayExplode = explode('/', $linkArray);
-$pageName = end($linkArrayExplode);
-?>
