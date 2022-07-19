15897244db163b83f30e4942ca344b4646f2b8d7
anastasiiaal <anastasia.alexeenko@gmail.com>
2022-07-19 14:29:45 +0200

    :sunflower: lil reorganization

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php
-require_once 'model/MovieDB.php';    
-require_once 'c:/wamp64/www/.gitignore/_api.php';
-$movieDB = new MovieDB($API_KEY);
+    header('location: /WhatTheFilm/views/home.php');
 ?>
