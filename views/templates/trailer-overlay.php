e702b985d5678e5d365264d7daa91d8bd5aba048
anastasiiaal <anastasia.alexeenko@gmail.com>
2022-07-25 11:34:54 +0200

    :rewind: trailer stops on close modal

--- views/templates/trailer-overlay.php
+++ views/templates/trailer-overlay.php
@@ -4,7 +4,7 @@
             <line x1="30.4142" y1="2.14273" x2="2.12994" y2="30.427" stroke="#EEEEEE" stroke-width="4"/>
             <line x1="30.9998" y1="30.2013" x2="2.71558" y2="1.91699" stroke="#EEEEEE" stroke-width="4"/>
         </svg>
-        <iframe width="560" height="315" src="<?= $getMovie['videos'] ?>" title="???" class="iframe" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        <iframe width="560" height="315" src="<?= $getMovie['videos'] ?>" title="???" class="iframe" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen id="ytvideo">
         </iframe>
     </div>
 </div>
